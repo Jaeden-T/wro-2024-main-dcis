9156773a4eaad663286dad3f40d039dd5f00f46f
Jaeden <95214533+Jaeden-T@users.noreply.github.com>
2024-09-05 06:32:05 +0800

    visual notifs

--- motor-pi/main.py
+++ motor-pi/main.py
@@ -3,9 +3,17 @@ import requests
 from c1.main import challenge1Movement
 calibrate = requests.get("http://pi3Sense.local:8000/calibrate")
 print(calibrate)
+import gpiozero as gpio
+
+servo= gpio.AngularServo(27, min_angle=0, max_angle=90, initial_angle=45)
+
 button = Button(26)
 mode = Button(22)
 
+servo.angle = 45
+servo.angle = 70
+servo.angle = 20
+servo.close()
 button.wait_for_press()
 print("Button pressed")
 if mode.is_pressed== True:
