5adcb9e8eee2b5184999314a7ccf0551059d98df
Jaeden <95214533+Jaeden-T@users.noreply.github.com>
2024-09-04 20:52:19 +0800

    fix double motors

--- motor-pi/c1/main.py
+++ motor-pi/c1/main.py
@@ -22,7 +22,6 @@ logging.basicConfig(
 #* Decaluartion
 # Motor = Motor, need set enable
 # Servo= angular serv
-motor = gpio.Motor(forward=0,backward=0,enable=0)
 servo= gpio.AngularServo(17, min_angle=0, max_angle=90, initial_angle=45)
 
 #* carSpeeds
